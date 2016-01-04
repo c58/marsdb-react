07802b7069c28aec5fea6e2a22b2453ccf02059c
Artem Artemyev <aartemev@naumen.ru>
2016-01-04 14:42:20 +0500

    added ExecutionContext comments

---
+++ test/both/ExecutionContext.test.js
@@ -0,0 +1,46 @@
+import Collection, { CursorObservable } from 'marsdb';
+import Cursor from 'marsdb/dist/Cursor';
+import * as utils from '../../lib/utils';
+import ExecutionContext from '../../lib/ExecutionContext';
+import chai, {expect} from 'chai';
+import sinon from 'sinon';
+chai.use(require('chai-as-promised'));
+chai.use(require('sinon-chai'));
+chai.should();
+
+
+describe('ExecutionContext', function () {
+
+  describe('#addCleanupListener', function () {
+
+  });
+
+  describe('#emitCleanup', function () {
+
+  });
+
+  describe('#createChildContext', function () {
+
+  });
+
+  describe('#withinContext', function () {
+
+  });
+
+  describe('#getVariables', function () {
+
+  });
+
+  describe('#trackVariablesChange', function () {
+
+  });
+
+  describe('#trackCursorChange', function () {
+
+  });
+
+  describe('#getCurrentContext', function () {
+
+  });
+
+});
---
+++ test/both/createContainer.test.js
@@ -0,0 +1,14 @@
+import Collection, { CursorObservable } from 'marsdb';
+import Cursor from 'marsdb/dist/Cursor';
+import * as utils from '../../lib/utils';
+import ExecutionContext from '../../lib/ExecutionContext';
+import chai, {expect} from 'chai';
+import sinon from 'sinon';
+chai.use(require('chai-as-promised'));
+chai.use(require('sinon-chai'));
+chai.should();
+
+
+describe('createContainer', function () {
+
+});
